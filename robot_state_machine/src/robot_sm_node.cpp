12294d3d348e9a2a80b4cb3f323860bba701a7d4
gopan-daptik <gopan@daptik.co>
2024-09-13 18:45:44 +0530

    zeromq update and docker file update

--- robot_state_machine/src/robot_sm_node.cpp
+++ robot_state_machine/src/robot_sm_node.cpp
@@ -6,10 +6,15 @@
 
 #include "../msg/diagnostics.pb.h"
 
+#include <zmq.hpp>
+
 #include <geometry_msgs/Pose.h>
 
 // Global state machine instance
 RobotStateMachine* robot_state_machine;
+
+zmq::context_t context(1);
+zmq::socket_t subscriber(context, ZMQ_SUB);
 
 void signalHandler(int signum) {
     robot_state_machine->process_event(Stop());
@@ -41,6 +46,27 @@ void diagnosticsCallback(const std_msgs::String::ConstPtr& msg) {
     }
 }
 
+void diagnosticsCallback(const std::string& msg) {
+    DiagnosticsMessage diag_msg;
+    diag_msg.ParseFromString(msg); //deserialise protobuf string
+
+    if (diag_msg.status() == DiagnosticsMessage::ERROR) {
+        robot_state_machine->process_event(ErrorDetected());
+    } else if (diag_msg.status() == DiagnosticsMessage::OK ) {
+        robot_state_machine->process_event(ErrorCleared());
+    }
+}
+
+void initZeroMQ() {
+    // Connect to the publisher (same address used for the publisher)
+    subscriber.connect("tcp://localhost:5556");
+
+    // Subscribe to all topics (empty subscription means all messages)
+    subscriber.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+
+    std::cout << "Subscribed to ZeroMQ messages on tcp://localhost:5556" << std::endl;
+}
+
 int main(int argc, char** argv) {
     ros::init(argc, argv, "robot_sm_node");
     ros::NodeHandle nh;
@@ -49,6 +75,7 @@ int main(int argc, char** argv) {
 
     robot_state_machine = &state_machine;
     state_machine.initiate();
+    initZeroMQ();
 
     //update path using rospkg find
     WPReader wp_reader("/home/anthar/ros_space/src/ground_vehicle_assignment/robot_state_machine/waypoints/wps.csv");
@@ -56,8 +83,17 @@ int main(int argc, char** argv) {
 
     // Set up ROS subscribers
     ros::Subscriber goal_sub = nh.subscribe("goal_topic", 10, goalCallback);
-    ros::Subscriber diagnostics_sub = nh.subscribe("diagnostics", 10, diagnosticsCallback);
+    //ros::Subscriber diagnostics_sub = nh.subscribe("diagnostics", 10, diagnosticsCallback);
 
-    ros::spin();
+    ros::Rate rate(100);
+    while (true) {
+        zmq::message_t message;
+        subscriber.recv(&message);
+        std::string message_str(static_cast<char*>(message.data()), message.size());
+        diagnosticsCallback(message_str);
+        //std::cout << "Received message: " << message_str << std::endl;
+        ros::spinOnce();
+        rate.sleep();
+    }
     return 0;
 }
